9441e26f8b520c6518f78de4b4afbbb0dfc68f5e
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-02-23 22:56:19 +0100

    Wish me luck on dis one

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ class Game {
     constructor(width = window.innerWidth, height = window.innerHeight) {
         this.width = width;
         this.height = height;
+        this.debug = false;
         this.player = new Player();
     }
 }
